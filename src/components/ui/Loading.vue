589ebdd92400c14b1b623a127ed0a97e905e1c8f
Roustaei <mahdieh.roustaei@gmail.com>
2021-01-27 11:19:49 +0100

    Loading

--- src/components/ui/Loading.vue
+++ src/components/ui/Loading.vue
@@ -1,30 +1,46 @@
 <template>
   <teleport to="body">
     <div class="loading">
-      <div><slot></slot></div>
-      <div class="spinner"><font-awesome-icon icon="spinner" /></div>
+      <div class="load-background"></div>
+      <div class="load-text">
+        <div><slot></slot></div>
+        <div class="spinner"><font-awesome-icon icon="spinner" /></div>
+      </div>
     </div>
   </teleport>
 </template>
-<style scoped>
+<style lang="scss" scoped>
 .loading {
   position: fixed;
   z-index: 100;
-  background-color: #000;
-  display: flex;
   width: 100%;
   height: 100%;
-  color: #fff;
+  color: $main-background;
   top: 0;
+}
+
+.load-background {
+  background-color: $body;
   opacity: 40%;
+  width: 100%;
+  height: 100%;
+}
+
+.load-text {
+  position: absolute;
+  display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
+  top: 40%;
+  left: 50%;
+  margin-left: -100px;
 }
 
 .spinner {
   animation: loader 2s linear infinite;
   font-size: 5rem;
+  width: 5rem;
 }
 
 @keyframes loader {
