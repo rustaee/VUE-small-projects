a69f841d07e2f8a5d52afd594e53a230723060cd
Roustaei <mahdieh.roustaei@gmail.com>
2021-02-08 20:40:16 +0100

    effects home page

--- src/pages/EffectsHome.vue
+++ src/pages/EffectsHome.vue
@@ -6,7 +6,31 @@
           <div class="effect">
             <router-link to="/contact">
               <font-awesome-icon icon="envelope-open-text" />
-              Effect Name goes here!
+              <span> Effect Name goes here!</span>
+            </router-link>
+          </div>
+          <div class="effect">
+            <router-link to="/contact">
+              <font-awesome-icon icon="envelope-open-text" />
+              <span> Effect Name goes here!</span>
+            </router-link>
+          </div>
+          <div class="effect">
+            <router-link to="/contact">
+              <font-awesome-icon icon="envelope-open-text" />
+              <span> Effect Name goes here!</span>
+            </router-link>
+          </div>
+          <div class="effect">
+            <router-link to="/contact">
+              <font-awesome-icon icon="envelope-open-text" />
+              <span> Effect Name goes here!</span>
+            </router-link>
+          </div>
+          <div class="effect">
+            <router-link to="/contact">
+              <font-awesome-icon icon="envelope-open-text" />
+              <span> Effect Name goes here!</span>
             </router-link>
           </div>
         </div>
@@ -14,7 +38,41 @@
     </section>
     <section class="light-bg">
       <div class="content">
-        lkjuhfslkdsf
+        <h2>My Favorites</h2>
+        <div class="my-favorites">
+          <div class="fav-effect">
+            <img src="@/assets/funicon.jpg" alt="" />
+            <div class="name">Effect Title</div>
+            <div class="desc">A short description</div>
+            <router-link :to="{ name: 'Effects' }"
+              ><button class="base-btn">Show me!</button></router-link
+            >
+          </div>
+          <div class="fav-effect">
+            <img src="@/assets/funicon.jpg" alt="" />
+            <div class="name">Effect Title</div>
+            <div class="desc">A short description</div>
+            <router-link :to="{ name: 'Effects' }"
+              ><button class="base-btn">Show me!</button></router-link
+            >
+          </div>
+          <div class="fav-effect">
+            <img src="@/assets/funicon.jpg" alt="" />
+            <div class="name">Effect Title</div>
+            <div class="desc">A short description</div>
+            <router-link :to="{ name: 'Effects' }"
+              ><button class="base-btn">Show me!</button></router-link
+            >
+          </div>
+          <div class="fav-effect">
+            <img src="@/assets/funicon.jpg" alt="" />
+            <div class="name">Effect Title</div>
+            <div class="desc">A short description</div>
+            <router-link :to="{ name: 'Effects' }"
+              ><button class="base-btn">Show me!</button></router-link
+            >
+          </div>
+        </div>
       </div>
     </section>
   </div>
@@ -33,6 +91,7 @@
   .content {
     width: 80%;
     padding: 20px 0;
+    text-align: center;
   }
 }
 
@@ -49,6 +108,84 @@
 
   .content {
     color: $effect-light-font-color;
+  }
+}
+
+.effects-list {
+  @extend %flex-row;
+
+  .effect {
+    margin: 20px 3rem;
+    position: relative;
+
+    a {
+      color: $effect-light-bg;
+
+      svg {
+        color: $effect-light-font-color;
+        margin-right: 10px;
+        font-size: 2rem;
+        position: absolute;
+        bottom: 0;
+      }
+
+      span {
+        position: relative;
+        left: 2.7rem;
+      }
+
+      &:hover {
+        color: $effect-base-color;
+
+        svg {
+          color: $effect-base-color;
+          animation: rotate 1.2s ease infinite;
+        }
+      }
+    }
+  }
+}
+
+@keyframes rotate {
+  0% {
+    transform: rotate(0deg);
+  }
+  12% {
+    transform: rotate(45deg);
+  }
+  25% {
+    transform: rotate(90deg);
+  }
+  37% {
+    transform: rotate(135deg);
+  }
+  50% {
+    transform: rotate(180deg);
+  }
+  62% {
+    transform: rotate(225deg);
+  }
+  75% {
+    transform: rotate(270deg);
+  }
+  87% {
+    transform: rotate(315deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+.my-favorites{
+  @extend %flex-row;
+
+  .fav-effect{
+
+    img{
+      width: 70px;
+      height: 70px;
+      @include border-radius(50%);
+    }
   }
 }
 </style>
